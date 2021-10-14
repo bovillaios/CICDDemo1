ecc6dd3e9cf1da64bb3991e29619556973ed3fca
Ram <ramgopal.bovilla@zopsmart.com>
2021-10-14 14:59:32 +0530

    project setup

---
+++ CICDDemo1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDDemo1
+//
+//  Created by Ramgopal on 14/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
